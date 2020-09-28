4096f3aef6eca2e03392620c4b4d87d4e8e16589
emrys57 <e.williams@linkz-im.com>
2020-09-28 12:42:14 +0100

    first commit

---
+++ src/box.jsx
@@ -0,0 +1,15 @@
+// Make a box to draw in.
+import React from 'react';
+
+export const Box = _props => {
+  return (
+    <div style={{
+      width: "80vw", height: '80vh',
+      backgroundColor: "lightblue",
+      marginLeft: '10vw', marginTop: '10vh',
+      border: '2px solid black'
+    }} >
+
+    </div>
+  );
+};
