a049f1c37e7a706a603be7e27d87892cc32748fd
convolve-midhunmadhavan <72149348+convolve-midhunmadhavan@users.noreply.github.com>
2020-10-01 07:44:56 +0530

    Initial commit
    All changes to add circles and drag them, and retain the position and number of circles.

--- src/box.jsx
+++ src/box.jsx
@@ -1,15 +1,25 @@
+
 // Make a box to draw in.
 import React from 'react';
+import Draggable from './draggable';
 
 export const Box = _props => {
+  var draggables = [];
+  for (var i = 1; i <= _props.circles; i++) {
+    var xValue = parseInt(localStorage.getItem('drag' + i + '-x'));
+    if (isNaN(xValue)) { xValue = 50; }
+    var yValue = parseInt(localStorage.getItem('drag' + i + '-y'));
+    if (isNaN(yValue)) { yValue = 50; }
+    draggables.push(<Draggable key={i} name={'drag' + i} position={{ x: xValue, y: yValue }} radius={20} />);
+  };
   return (
     <div style={{
       width: "80vw", height: '80vh',
       backgroundColor: "lightblue",
-      marginLeft: '10vw', marginTop: '10vh',
-      border: '2px solid black'
+      marginLeft: '10vw', marginTop: '5vh',
+      border: '2px solid black',  
     }} >
-
+      {draggables}
     </div>
   );
 };
